099251618dcc059e42b46499374cdf417c19f7e2
moni8919 <chinthaparthimounika050@gmail.com>
2024-12-13 15:46:49 +0530

    Add files via upload

---
+++ algomanicfinals.java
@@ -0,0 +1,17 @@
+import java.util.*;
+import java.lang.*;
+import java.io.*;
+
+class Codechef
+{
+	public static void main (String[] args) throws java.lang.Exception
+	{
+		
+		Scanner sc=new Scanner(System.in);
+		int X=sc.nextInt();
+		if(X==17)
+		System.out.println("yay");
+		else
+		System.out.println("no");
+	}
+}
