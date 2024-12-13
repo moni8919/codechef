d0eb2e2dc0da56c1423674d1bb159c4e396610b4
moni8919 <chinthaparthimounika050@gmail.com>
2024-12-13 15:47:29 +0530

    Add files via upload

---
+++ chefsocks.java
@@ -0,0 +1,21 @@
+import java.util.*;
+import java.lang.*;
+import java.io.*;
+
+class Codechef
+{
+	public static void main (String[] args) throws java.lang.Exception
+	{
+	Scanner sc=new Scanner(System.in);
+	int A= sc.nextInt();
+	int X= sc.nextInt();
+	int Y=sc.nextInt();
+	if(A<=X+Y)
+	{
+	    System.out.println("YES");
+	}else{
+	    System.out.println("NO");
+	}
+
+	}
+}
