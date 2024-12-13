88018100ca59a3531d86fa5c557dbb7b8501f215
moni8919 <chinthaparthimounika050@gmail.com>
2024-12-13 15:54:33 +0530

    Add files via upload

---
+++ summertime.java
@@ -0,0 +1,18 @@
+import java.util.*;
+import java.lang.*;
+import java.io.*;
+
+class Codechef
+{
+	public static void main (String[] args) throws java.lang.Exception
+	{
+		Scanner sc=new Scanner(System.in);
+		int x=sc.nextInt();
+		if(x>35)
+		{
+		    System.out.println("yes");
+		}else{
+		    System.out.println("no");
+		}
+	}
+}
