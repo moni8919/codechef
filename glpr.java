eecc41713c81862578563f9abc669f7da4277ef1
moni8919 <chinthaparthimounika050@gmail.com>
2024-12-13 15:48:50 +0530

    Add files via upload

---
+++ glpr.java
@@ -0,0 +1,20 @@
+import java.util.*;
+import java.lang.*;
+import java.io.*;
+
+class Codechef
+{
+	public static void main (String[] args) throws java.lang.Exception
+	{
+		Scanner sc=new Scanner(System.in);
+		int X=sc.nextInt();
+		int Y=sc.nextInt();
+		if(2*X>=Y)
+		{
+		    System.out.println("METAL");
+		    
+		}else{
+		    System.out.println("PLASTIC");
+		}
+	}
+}
