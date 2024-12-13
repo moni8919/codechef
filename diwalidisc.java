d5e3e9e5566593e522c5e6ebb18926d93ce81676
moni8919 <chinthaparthimounika050@gmail.com>
2024-12-13 15:48:08 +0530

    Add files via upload

---
+++ diwalidisc.java
@@ -0,0 +1,18 @@
+import java.util.*;
+import java.lang.*;
+import java.io.*;
+
+class Codechef
+{
+	public static void main (String[] args) throws java.lang.Exception
+	{
+	 Scanner sc=new Scanner(System.in);
+	 int a=sc.nextInt();
+	 int b=sc.nextInt();
+	 if(a>=b)
+	 System.out.println(a-b);
+	 else
+	 System.out.println(0);
+
+	}
+}
