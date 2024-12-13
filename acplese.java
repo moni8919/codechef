22baa86500391de6f537280e2de9e03ce0095966
moni8919 <chinthaparthimounika050@gmail.com>
2024-12-13 15:46:02 +0530

    Add files via upload

---
+++ acplese.java
@@ -0,0 +1,16 @@
+import java.util.*;
+import java.lang.*;
+import java.io.*;
+
+class Codechef
+{
+	public static void main (String[] args) throws java.lang.Exception
+	{
+		Scanner sc=new Scanner(System.in);
+		int t=sc.nextInt();
+		if(t>30)
+		System.out.println("Yes");
+		else
+		System.out.println("No");
+	}
+}
