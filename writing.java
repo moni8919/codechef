d3217c034163af175561c665f5ad49135f9db649
moni8919 <chinthaparthimounika050@gmail.com>
2024-12-13 15:55:34 +0530

    Add files via upload

---
+++ writing.java
@@ -0,0 +1,17 @@
+import java.util.*;
+import java.lang.*;
+import java.io.*;
+
+class Codechef
+{
+	public static void main (String[] args) throws java.lang.Exception
+	{
+		Scanner sc=new Scanner(System.in);
+		int X=sc.nextInt();
+		if(5*X<=60)
+		System.out.println("YES");
+		else
+		System.out.println("NO");
+
+	}
+}
