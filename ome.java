925372e7744673ca5263570cd04007392bb4da0e
moni8919 <chinthaparthimounika050@gmail.com>
2024-12-13 15:53:05 +0530

    Add files via upload

---
+++ ome.java
@@ -0,0 +1,20 @@
+import java.util.*;
+import java.lang.*;
+import java.io.*;
+
+class Codechef
+{
+	public static void main (String[] args) throws java.lang.Exception
+	{
+	  Scanner sc=new Scanner(System.in);
+	  int t=sc.nextInt();
+	  while(t-->0){
+	  int X=sc.nextInt();
+	  if(X>24)
+	  System.out.println("yes");
+	  else
+	  System.out.println("no");
+	  }
+
+	}
+}
