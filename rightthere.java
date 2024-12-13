ffa30986d7df410c9987bba77e613ec478431bbd
moni8919 <chinthaparthimounika050@gmail.com>
2024-12-13 15:53:54 +0530

    Add files via upload

---
+++ rightthere.java
@@ -0,0 +1,22 @@
+import java.util.*;
+import java.lang.*;
+import java.io.*;
+
+class Codechef
+{
+	public static void main (String[] args) throws java.lang.Exception
+	{
+	    Scanner sc=new Scanner(System.in);
+    	int t=sc.nextInt();
+    	while(t-->0){
+    	    int x=sc.nextInt();
+    	    int n=sc.nextInt();
+    	    if(x<=n)
+    	    System.out.println("yes");
+    	    else
+    	    System.out.println("no");
+    	}
+	
+
+	}
+}
