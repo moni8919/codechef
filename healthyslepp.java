9906c884084346859769e7f441207d41ea5909cc
moni8919 <chinthaparthimounika050@gmail.com>
2024-12-13 15:49:54 +0530

    Add files via upload

---
+++ healthyslepp.java
@@ -0,0 +1,25 @@
+import java.util.*;
+import java.lang.*;
+import java.io.*;
+
+class Codechef
+{
+	public static void main (String[] args) throws java.lang.Exception
+	{
+	    Scanner sc=new Scanner(System.in);
+		int x=sc.nextInt();
+		if(x<8)
+		{
+		    System.out.println("less");
+		}
+		else if(x==8)
+		{
+		    System.out.println("perfect");
+		}
+		else
+		{
+		    System.out.println("more");
+		}
+
+	}
+}
