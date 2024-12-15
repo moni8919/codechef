09d05f2c155dfb51720b8204f402318879795e15
moni8919 <chinthaparthimounika050@gmail.com>
2024-12-15 12:19:09 +0530

    Add files via upload

---
+++ courseregistration.java
@@ -0,0 +1,24 @@
+import java.util.*;
+import java.lang.*;
+import java.io.*;
+
+class Codechef
+{
+	public static void main (String[] args) throws java.lang.Exception
+	{
+	Scanner sc=new Scanner(System.in);
+	int t=sc.nextInt();
+	while(t-->0){
+	    	int n=sc.nextInt();
+	    	int m=sc.nextInt();
+	    	int k=sc.nextInt();
+	    	int z=n+k;
+	    	if(z<=m)
+	    	System.out.println("yes");
+	    	else
+	    	System.out.println("no");
+	    
+	}
+
+	}
+}
