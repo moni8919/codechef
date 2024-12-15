84f56939d9aa6f7ad655b923ea3d396464077076
moni8919 <chinthaparthimounika050@gmail.com>
2024-12-15 11:44:07 +0530

    Add files via upload

---
+++ cabs.java
@@ -0,0 +1,23 @@
+import java.util.*;
+import java.lang.*;
+import java.io.*;
+
+class Codechef
+{
+	public static void main (String[] args) throws java.lang.Exception
+	{
+		Scanner sc=new Scanner(System.in);
+		int t=sc.nextInt();
+		for(int i=0;i<t;i++){
+		    int x=sc.nextInt();
+		    int y=sc.nextInt();
+		    if(x<y)
+		    System.out.println("first");
+		    else if(x>y)
+		    System.out.println("second");
+		    else
+		    System.out.println("any");
+		}
+
+	}
+}
