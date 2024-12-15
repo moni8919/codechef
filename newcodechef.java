3eac8779f06e104ebaa5b1f502e7dd5007e0ceda
moni8919 <chinthaparthimounika050@gmail.com>
2024-12-15 11:46:04 +0530

    Add files via upload

---
+++ newcodechef.java
@@ -0,0 +1,21 @@
+import java.util.*;
+import java.lang.*;
+import java.io.*;
+
+class Codechef
+{
+	public static void main (String[] args) throws java.lang.Exception
+	{
+		Scanner sc=new Scanner(System.in);
+        int x=sc.nextInt();
+        int y=sc.nextInt();
+        if(x<y)
+        	System.out.println("old");
+        else if(x>y)
+        	System.out.println("new");
+        else
+        	System.out.println("same");
+        
+
+	}
+}
