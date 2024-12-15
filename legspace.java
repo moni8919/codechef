088571cd16651d0d3d015c4409924ac2e012444e
moni8919 <chinthaparthimounika050@gmail.com>
2024-12-15 11:45:35 +0530

    Add files via upload

---
+++ legspace.java
@@ -0,0 +1,18 @@
+import java.util.*;
+import java.lang.*;
+import java.io.*;
+
+class Codechef
+{
+	public static void main (String[] args) throws java.lang.Exception
+	{
+		Scanner sc=new Scanner(System.in);
+		int n=sc.nextInt();
+		int m=sc.nextInt();
+		if(m>n)
+		System.out.println("yes");
+		else
+		System.out.println("no");
+
+	}
+}
