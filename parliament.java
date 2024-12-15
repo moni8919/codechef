29d44a1e658fcb283131c8e19520a7c7d3d98c5f
moni8919 <chinthaparthimounika050@gmail.com>
2024-12-15 11:46:41 +0530

    Add files via upload

---
+++ parliament.java
@@ -0,0 +1,19 @@
+import java.util.*;
+import java.lang.*;
+import java.io.*;
+
+class Codechef
+{
+	public static void main (String[] args) throws java.lang.Exception
+	{
+		Scanner sc=new Scanner(System.in);
+		int t=sc.nextInt();
+		while(t-->0){
+		    byte n=sc.nextByte();
+		    byte x=sc.nextByte();
+		    System.out.println(x>=n/2.0 ? "YES" : "NO" );
+		
+		}
+	}
+}
+	
